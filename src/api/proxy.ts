ad87bb06f4a31e15757bee6df4c546eb2121cb96
Miika Henttonen <miika.henttonen@kipina.fi>
2023-12-16 19:48:35 +0200

    Add password auth

--- src/api/proxy.ts
+++ src/api/proxy.ts
@@ -2,9 +2,75 @@ import { Request } from "express";
 import http from "http";
 import { app } from "../express";
 
-const { MUSA_BASE_URL = "" } = process.env;
+const { MUSA_BASE_URL = "", PASSWORD } = process.env;
+
+const loginHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Musa Login</title>
+</head>
+
+<body>
+  <form>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" placeholder="Password" required>
+
+    <button type="button" onclick="login()">Login</button>
+  </form>
+
+  <script>
+    function login() {
+      var passwordInput = document.getElementById("password");
+      var password = passwordInput.value;
+    
+      fetch("/login", {
+        method: "POST",
+        headers: {
+          authorization: "Secret " + password,
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.text());
+          }
+          return response.text();
+        })
+        .then((data) => {
+          window.location.href = "/";
+        })
+        .catch((error) => {
+          console.error("Error during login:", error.message);
+        });
+    }
+    </script>
+</body>
+
+</html>
+`;
+
+const allowListForFiles = ["manifest.json"];
 
 app.use("/(.*)", async (req: Request<{ id: string }>, res) => {
+  if (req.originalUrl === "/login") {
+    const password = req.headers.authorization?.split("Secret ").pop() ?? "";
+    res.cookie("musaPassword", password, { httpOnly: true });
+    res.status(200).send("Ok");
+    return;
+  }
+
+  const isAllowed = allowListForFiles.some((file) => {
+    return req.originalUrl.includes(file);
+  });
+  const password = req.cookies.musaPassword;
+  if (!isAllowed && (!password || password !== PASSWORD)) {
+    res.status(401).send(loginHtml);
+    return;
+  }
+
   const body = JSON.stringify(req.body);
   const url = `${MUSA_BASE_URL}${req.originalUrl}`;
   console.log("Calling", req.method, url);
--- src/express.ts
+++ src/express.ts
@@ -1,9 +1,11 @@
 import compression from "compression";
 import cors from "cors";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 export const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(compression());
+app.use(cookieParser());
