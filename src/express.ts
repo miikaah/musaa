9e9b7bd814f42b3495205071e670b04bf11043a9
Miika Henttonen <miika.henttonen@kipina.fi>
2023-12-16 20:06:35 +0200

    Revert "Add helmet"
    This reverts commit 2d38fd6ecf2abe5524b1ec6a262af2a451270a87.

--- src/express.ts
+++ src/express.ts
@@ -2,7 +2,6 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
-import helmet from "helmet";
 
 export const app = express();
 
@@ -10,12 +9,3 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(compression());
 app.use(cookieParser());
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-    crossOriginOpenerPolicy: false,
-    crossOriginResourcePolicy: false,
-    crossOriginEmbedderPolicy: false,
-    originAgentCluster: false,
-  })
-);
