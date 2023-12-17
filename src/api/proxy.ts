c226ecc17b093eab4c90be46db381e1cf473a992
Miika Henttonen <miika.henttonen@kipina.fi>
2023-12-17 10:49:59 +0200

    Refactor to use form data

--- src/api/proxy.ts
+++ src/api/proxy.ts
@@ -7,107 +7,86 @@ const { MUSA_BASE_URL = "", PASSWORD } = process.env;
 const loginHtml = `
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musa Login</title>
+  </head>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Musa Login</title>
-</head>
+  <body>
+    <form action="/login" method="POST">
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" placeholder="Password" required>
 
-<body>
-  <form>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password" placeholder="Password" required>
-
-    <button type="button" onclick="login()">Login</button>
-  </form>
-
-  <script>
-    function login() {
-      var passwordInput = document.getElementById("password");
-      var password = passwordInput.value;
-    
-      fetch("/login", {
-        method: "POST",
-        headers: {
-          authorization: "Secret " + password,
-        },
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(response.text());
-          }
-          return response.text();
-        })
-        .then((data) => {
-          window.location.href = "/";
-        })
-        .catch((error) => {
-          console.error("Error during login:", error.message);
-        });
-    }
-    </script>
-</body>
-
+      <button type="submit">Login</button>
+    </form>
+  </body>
 </html>
 `;
 
-const allowListForFiles = ["manifest.json"];
+const allowListForFiles = ["manifest.json", "favicon.ico"];
 
-app.use("/(.*)", async (req: Request<{ id: string }>, res) => {
-  if (req.originalUrl === "/login") {
-    const password = req.headers.authorization?.split("Secret%20").pop() ?? "";
-    res.cookie("musaPassword", password, { httpOnly: true });
-    res.status(200).send("Ok");
-    return;
-  }
+app.use(
+  "/(.*)",
+  async (req: Request<{ id: string }, unknown, { password: string }>, res) => {
+    if (req.originalUrl === "/login") {
+      const password = req.body.password;
 
-  const isAllowed = allowListForFiles.some((file) => {
-    return req.originalUrl.includes(file);
-  });
-  const password = decodeURI(req.cookies.musaPassword);
-  if (!isAllowed && (!password || password !== PASSWORD)) {
-    res.status(401).send(loginHtml);
-    return;
-  }
+      if (!password || password !== PASSWORD) {
+        res.status(401).send(loginHtml);
+        return;
+      }
 
-  const body = JSON.stringify(req.body);
-  const url = `${MUSA_BASE_URL}${req.originalUrl}`;
+      res.cookie("musaPassword", password, { httpOnly: true });
+      res.status(200).redirect("/");
+      return;
+    }
 
-  req.url = url;
+    const isAllowed = allowListForFiles.some((file) => {
+      return req.originalUrl.includes(file);
+    });
+    const password = req.cookies.musaPassword;
+    if (!isAllowed && (!password || password !== PASSWORD)) {
+      res.status(401).send(loginHtml);
+      return;
+    }
 
-  // Create options for the outgoing request
-  const options: http.RequestOptions = {
-    method: req.method,
-    headers: {
-      ...req.headers,
-      "content-length": req.body ? Buffer.byteLength(body) : 0,
-      "x-musa-proxy": "yes",
-    },
-  };
+    const body = JSON.stringify(req.body);
+    const url = `${MUSA_BASE_URL}${req.originalUrl}`;
 
-  const outgoingRequest = http.request(url, options, (proxyRes) => {
-    // Forward the response headers
-    res.set(proxyRes.headers);
-    // Forward status code (enables caching for the browser)
-    res.status(proxyRes.statusCode ?? 200);
-    res.statusMessage = proxyRes.statusMessage ?? "";
+    // Create options for the outgoing request
+    const options: http.RequestOptions = {
+      method: req.method,
+      headers: {
+        ...req.headers,
+        "content-length": req.body ? Buffer.byteLength(body) : 0,
+        "x-musa-proxy": "yes",
+      },
+    };
 
-    // Pipe the response from the target endpoint to the original response
-    proxyRes.pipe(res);
-  });
+    const outgoingRequest = http.request(url, options, (proxyRes) => {
+      // Forward the response headers
+      res.set(proxyRes.headers);
+      // Forward status code (enables caching for the browser)
+      res.status(proxyRes.statusCode ?? 200);
+      res.statusMessage = proxyRes.statusMessage ?? "";
 
-  // Forward the request body to the target endpoint
-  if (req.body) {
-    outgoingRequest.write(body);
-  }
+      // Pipe the response from the target endpoint to the original response
+      proxyRes.pipe(res);
+    });
 
-  outgoingRequest.on("error", (err) => {
-    console.error(`Error making proxy request: ${err.message}`);
+    // Forward the request body to the target endpoint
+    if (req.body) {
+      outgoingRequest.write(body);
+    }
 
-    res.status(500).send("Internal Server Error");
-  });
+    outgoingRequest.on("error", (err) => {
+      console.error(`Error making proxy request: ${err.message}`);
 
-  // End the request to the target endpoint
-  outgoingRequest.end();
-});
+      res.status(500).send("Internal Server Error");
+    });
+
+    // End the request to the target endpoint
+    outgoingRequest.end();
+  },
+);
--- src/express.ts
+++ src/express.ts
@@ -2,6 +2,7 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 export const app = express();
 
@@ -9,3 +10,4 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(compression());
 app.use(cookieParser());
+app.use(bodyParser.urlencoded());
